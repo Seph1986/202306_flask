bc2e97723612a3a6a8f39a19d3c2c7769e066486
Seph1986 <jose.weber97@hotmail.com>
2023-08-02 11:13:37 -0400

    first commit

---
+++ PRACTICE/1.Comprender enrutamiento/server.py
@@ -0,0 +1,20 @@
+from flask import Flask
+app = Flask(__name__)    
+@app.route('/')          
+def hola_mundo():
+    return 'Hola Mundo!' 
+
+@app.route('/dojo')
+def dojo():
+    return 'Dojo!'
+
+@app.route('/say/<string:par>')
+def greeting(par):
+    return f'¡Hola, {par}!'
+
+@app.route('/repeat/<int:count>/<string:word>')
+def repeat(count,word):
+    return f'{word*count}'
+
+if __name__=="__main__":  
+    app.run(debug=True)
