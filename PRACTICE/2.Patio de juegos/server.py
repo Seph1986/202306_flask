7bb68a49b4f5e1ccc49d31e185ba0caeb6bd0a11
Seph1986 <jose.weber97@hotmail.com>
2023-08-02 17:22:27 -0400

    Cajas de colores

---
+++ PRACTICE/2.Patio de juegos/server.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/play')
+def land_page():
+    return render_template('index.html')
+
+@app.route('/play/<int:count>')
+def number(count):
+    return render_template('counts.html', num = count)
+
+@app.route('/play/<int:count>/<color>')
+def estilo(count, color):
+    return render_template('color.html', num = count, estilo = color)
+
+
+if __name__=="__main__":  
+    app.run(debug=True)
