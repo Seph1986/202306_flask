6d060611d04fa46f2fefbc57f5ab8242f85a9c24
Seph1986 <jose.weber97@hotmail.com>
2023-08-07 19:37:39 -0400

    Ultimo trabajo core, muy divertido

---
+++ CORE/4.Encuesta Dojo/server.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, redirect, request, session
+
+app = Flask(__name__)
+app.secret_key = "keep it secret, keep it safe"
+
+@app.route("/")
+def landing():
+
+    return render_template("landing.html")
+
+@app.route("/result/")
+def result():
+
+    return render_template("info.html")
+
+@app.route("/form/", methods = ["POST"])
+def form_recive():
+
+    session["user_name"] = request.form["user_name"]
+    session["location"] = request.form["location"]
+    session["language"] = request.form["language"]
+    session["comment"] = request.form["comment"]
+
+    print(session["user_name"],session["location"],session["language"],session["comment"] )
+    
+
+    return redirect("/result/")
+
+@app.route("/reset/")
+def reset_user():
+
+    keys = ["user_name","location","language","comment"]
+
+    for key in keys:
+        del session[key]
+
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
